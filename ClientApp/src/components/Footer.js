9d2479b09f1edef3da87d3182ae30c1d1c719f12
Junyi Xie <yunyi.xie@outlook.com>
2023-12-06 23:21:42 +0100

    header border bottom hover

--- ClientApp/src/components/Footer.js
+++ ClientApp/src/components/Footer.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Container, NavItem, NavLink } from 'reactstrap';
+import { Container, Nav, NavItem, NavLink } from 'reactstrap';
 
 export class Footer extends Component {
     render() {
         return (
-            <footer>
-                <Container>
-                    <div className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                        <span className="col-md-5">&copy; {new Date().getFullYear()} Stichting Accessibility</span>
-                        <div className="col-md-2 d-flex justify-content-center">
-                            <Link to="/">
-                                <img src={require("../assets/img/brand/logo.png")} width="50" height="60" alt="Logo" title="Project Accessibility" />
-                            </Link>
-                        </div>
-                        <ul className="nav col-md-5 justify-content-end">
-                            <NavItem>
-                                <NavLink tag={Link} className="nav-link px-2" to="/sitemap">Sitemap</NavLink>
+            <footer className="site__footer">
+                <Container className="site__footer_container">
+                    <span className="site__footer_copyright">&copy; {new Date().getFullYear()} Stichting Accessibility</span>
+                    <div className="site__footer_logo">
+                        <Link to="/">
+                            <img src={require("../assets/img/brand/logo.png")} width="50" height="60" alt="Logo" title="Project Accessibility" />
+                        </Link>
+                    </div>
+                    <div className="site__footer_nav">
+                        <Nav className="site__footer_nav_list">
+                            <NavItem className="site__footer_nav_item">
+                                <NavLink tag={Link} className="site__footer_nav_link" to="/sitemap">Sitemap</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink tag={Link} className="nav-link px-2" to="/privacy-policy">Privacy Policy</NavLink>
+                                <NavLink tag={Link} className="site__footer_nav_link" to="/privacy-policy">Privacy Policy</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink tag={Link} className="nav-link px-2" to="/cookies">Cookies</NavLink>
+                                <NavLink tag={Link} className="site__footer_nav_link" to="/cookies">Cookies</NavLink>
                             </NavItem>
-                        </ul>
+                        </Nav>
                     </div>
                 </Container>
             </footer>
--- ClientApp/src/components/MobileNav.js
+++ ClientApp/src/components/MobileNav.js
@@ -3,26 +3,56 @@ import { Link } from 'react-router-dom';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 
 export class MobileNav extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false,
+        };
+        this.toggleMenuState = this.toggleMenuState.bind(this);
+    }
+
+    // need to fix this, after you open link, close menu
+    toggleMenuState() {
+        this.setState((prevState) => ({
+            isOpen: !prevState.isOpen,
+        }));
+        document.body.classList.toggle("site__menu_is__open");
+    }
+
     render() {
         return (
-            <div className="site__header_nav">
-                <Nav className="site__header_nav_list">
-                    <NavItem className="site__header_nav_item">
-                        <NavLink tag={Link} className="site__header_nav_link" to="/">Home</NavLink>
-                    </NavItem>
-                    <NavItem className="site__header_nav_item">
-                        <NavLink tag={Link} className="site__header_nav_link" to="/over-ons">Over ons</NavLink>
-                    </NavItem>
-                    <NavItem className="site__header_nav_item">
-                        <NavLink tag={Link} className="site__header_nav_link" to="/expertise">Expertise</NavLink>
-                    </NavItem>
-                    <NavItem className="site__header_nav_item">
-                        <NavLink tag={Link} className="site__header_nav_link" to="/blog">Actueel</NavLink>
-                    </NavItem>
-                    <NavItem className="site__header_nav_item">
-                        <NavLink tag={Link} className="site__header_nav_link" to="/contact">Contact</NavLink>
-                    </NavItem>
-                </Nav>
+            <div className="site__header_menu">
+                <input type="checkbox" className="site__header_menu_checkbox" id="site__header_menu_toggle" />
+                <label className="site__header_menu_button" htmlFor="site__header_menu_toggle" onClick={this.toggleMenuState}>
+                    <span className="site__header_menu_icon">
+                        <span className="site__header_menu_span" />
+                        <span className="site__header_menu_span" />
+                        <span className="site__header_menu_span" />
+                        <span className="site__header_menu_span" />
+                        <span className="site__header_menu_span" />
+                        <span className="site__header_menu_span" />
+                    </span>
+                </label>
+                <div className="site__header_menu_background" />
+                <div className="site__header_menu_nav">
+                    <Nav className="site__header_menu_list">
+                        <NavItem className="site__header_menu_item">
+                            <NavLink tag={Link} className="site__header_menu_link" to="/">Home</NavLink>
+                        </NavItem>
+                        <NavItem className="site__header_menu_item">
+                            <NavLink tag={Link} className="site__header_menu_link" to="/over-ons">Over ons</NavLink>
+                        </NavItem>
+                        <NavItem className="site__header_menu_item">
+                            <NavLink tag={Link} className="site__header_menu_link" to="/expertise">Expertise</NavLink>
+                        </NavItem>
+                        <NavItem className="site__header_menu_item">
+                            <NavLink tag={Link} className="site__header_menu_link" to="/blog">Actueel</NavLink>
+                        </NavItem>
+                        <NavItem className="site__header_menu_item">
+                            <NavLink tag={Link} className="site__header_menu_link" to="/contact">Contact</NavLink>
+                        </NavItem>
+                    </Nav>
+                </div>
             </div>
         );
     }
