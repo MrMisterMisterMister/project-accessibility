905ff5b1abbe69be9c451c4e0175e3b287609f8d
Junyi Xie <yunyi.xie@outlook.com>
2023-12-07 13:48:53 +0100

    hero update

--- ClientApp/src/components/Hero.js
+++ ClientApp/src/components/Hero.js
@@ -6,16 +6,16 @@ export class Hero extends Component {
     render() {
         return (
             <section className="site__hero" id="hero">
-                <Container>
-                    <div className="col-12 col-lg-6 ">
-                        <h1 className="mb-3">Lorem ipsum</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Proin libero nunc consequat interdum varius sit amet.</p>
-                        <Link to="/">
-                            <Button role="button" className="hero__button">
+                <Container className="site__hero_container">
+                    <div className="site__hero_wrapper">
+                        <h1 className="site__hero_title">Toegankelijkheidsportaal voor iedereen.</h1>
+                        <p className="site__hero_text">Een inclusieve samenleving waarin alle mensen met of zonder beperking gelijkwaardig participeren.</p>
+                        <Link to="/login">
+                            <Button role="button" className="site__hero_button">
                                 <span className="circle" aria-hidden="true">
                                     <span className="icon arrow" />
                                 </span>
-                                <span className="hero__text">Lorem ipsum</span>
+                                <span className="site__hero_button_text">Naar het portaal</span>
                             </Button>
                         </Link>
                     </div>
