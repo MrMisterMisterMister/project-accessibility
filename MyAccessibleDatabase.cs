25f13d3f53e29276e25345ef9197ee879cbe26df
Chris Yeung <jackknife29jk@gmail.com>
2023-12-09 14:28:28 +0100

    commit
    Added ef core packages, Database class, with temporary connection string to azure, in progress

---
+++ MyAccessibleDatabase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace project_accessibility
+{
+    public class MyAccessibleDatabase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Replace with your actual connection string
+                string my_password = "ikbendeadmin888!";
+                string connectionString = $"Server=tcp:myaccessibilityserver.database.windows.net,1433;Initial Catalog=AccessiblityDB;Persist Security Info=False;User ID=adminsa;Password={my_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+    }
+}
