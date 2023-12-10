6ef65fb676d2a44d08f01d331604babb2302b5e2
Junyi Xie <yunyi.xie@outlook.com>
2023-12-10 15:24:52 +0100

    background clip for text effect

--- ClientApp/src/components/Contact.js
+++ ClientApp/src/components/Contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
                 <div className="contact__section_wrapper">
                     <Shape section="contact" position={['bottom_left', 'top_right']} />
                     <div className="contact__section_content">
-                        <h2 className="contact__section_content__title">Meld je aan voor de onderzoeken</h2>
+                        <h2 className="contact__section_content__title">Meld je aan en <span className="contact__section_content__highlight">doe mee aan onderzoeken</span> in het toegankelijkheidsportaal</h2>
                         <p className="contact__section_content__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                         <div className="contact__section_content_button">
                             <ButtonContact style="signup" path="/" text="Sign up" />
