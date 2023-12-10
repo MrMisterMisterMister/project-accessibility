970f5ee80592167de4dedadb8996254017744b62
Junyi Xie <yunyi.xie@outlook.com>
2023-12-10 14:34:18 +0100

    Added some shapes

--- ClientApp/src/components/Contact.js
+++ ClientApp/src/components/Contact.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 import { ButtonContact } from './Button';
+import Shape from './Shape';
 
 const Contact = () => {
     return (
         <section className="contact__section">
             <Container className="contact__section_container">
                 <div className="contact__section_wrapper">
+                    <Shape section="contact" position={['bottom_left', 'top_right']} />
                     <div className="contact__section_content">
                         <h2 className="contact__section_content__title">Meld je aan voor de onderzoeken</h2>
                         <p className="contact__section_content__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
