58a45449ed41562c3b44104d131e02a68f702bde
Christopher Yeung <jackknife29jk@gmail.com>
2023-12-13 10:00:00 +0100

    added a logger for the LoginController class
    Added a logger for the LoginController class, and placed it in select actions.

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -8,21 +8,28 @@ namespace project_accessibility.Controllers
     public class LoginController : ControllerBase
     {
         private readonly MyAccessibleDatabase _db;
+        private readonly ILogger<LoginController> _logger;
 
-        public LoginController(MyAccessibleDatabase db)
+        public LoginController(MyAccessibleDatabase db, ILogger<LoginController> logger)
         {
             _db = db;
+            _logger = logger;
         }
 
         [HttpPost]
         public IActionResult Login([FromBody] LoginModel model)
         {
+            _logger.LogInformation("Attempting login for user: {Username}", model.Username);
+
             if (_db.IsValidUser(model.Username, model.Password))
             {
+                _logger.LogInformation("User {Username} logged in successfully", model.Username);
+
                 // Authentication successful, you can generate a token or set session information here
                 return Ok(new { message = "Login successful" });
             }
 
+            _logger.LogWarning("Failed login attempt for user: {Username}", model.Username);
             return Unauthorized(new { message = "Invalid credentials" });
         }
     }
