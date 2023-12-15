4f319010b38a6246961ce12465a3c18f54dc5c5b
Junyi Xie <yunyi.xie@outlook.com>
2023-12-15 13:41:23 +0100

    Deployment workflow

---
+++ ClientApp/deployment.sh
@@ -0,0 +1,24 @@
+# Check if the username is provided
+if [ -z "$1" ]; then
+  echo "Error: No username provided."
+  echo "Usage: ./deploymeny.sh [username]"
+  exit 1
+fi
+
+# Switch to the main branch
+echo "Switching to main branch..."
+git checkout main
+
+deployUser=$1
+
+echo "Building app.."
+npm run build
+
+echo "Deploying app.."
+scp -r build/* "$deployUser"@142.93.135.187:/var/www/clodsire.nl/html
+
+# requires ssh key
+echo "Restarting Nginx on the server.."
+ssh "$deployUser"@142.93.135.187 'sudo systemctl restart nginx'
+
+echo "Deployment finished.."
