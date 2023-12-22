b32c7d8c3aeadb74a1441952705d5b2f23943717
Junyi Xie <yunyi.xie@outlook.com>
2023-12-22 14:43:41 +0100

    last one

--- ClientApp/src/components/Contact.js
+++ ClientApp/src/components/Contact.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Container } from 'reactstrap';
+import { useTranslation } from 'react-i18next';
 import { ButtonContact } from './Button';
 
+// Contact component
 const Contact = () => {
+    // Translation
+    const { t: translate } = useTranslation();
+
     return (
         <section className="contact__section">
             <Container className="contact__section_container">
                 <div className="contact__section_wrapper">
                     <div className="contact__section_content">
-                        <h2 className="contact__section_content__title">Meld je aan en doe mee aan onderzoeken in het toegankelijkheidsportaal</h2>
-                        <p className="contact__section_content__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <h2 className="contact__section_content__title">{translate("contact.title")}</h2>
+                        <p className="contact__section_content__text">{translate("contact.description")}</p>
                         <div className="contact__section_content_button">
-                            <ButtonContact style="signup" path="/" text="Sign up" />
-                            <ButtonContact style="login" path="/" text="Login" />
+                            <ButtonContact style="signup" path="/" text={translate("contact.buttons.signup")} />
+                            <ButtonContact style="login" path="/" text={translate("contact.buttons.signin")} />
                         </div>
                     </div>
                 </div>
