848792889ef26abce0169061baee7b455fb29dff
Junyi Xie <yunyi.xie@outlook.com>
2023-12-25 23:49:31 +0100

    fix final eslint warning

--- projectaccessibility/src/components/Nav.jsx
+++ projectaccessibility/src/components/Nav.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 import { Nav } from "react-bootstrap";
 
 // Render hamburger menu for mobile menu
--- projectaccessibility/vite.config.js
+++ projectaccessibility/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: { 
+    server: {
         port: 5000
     },
     plugins: [react()]
