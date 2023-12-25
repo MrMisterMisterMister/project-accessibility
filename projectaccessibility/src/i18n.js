f01493ac84a1697eab8c69023da1a2d94093ad0e
Junyi Xie <yunyi.xie@outlook.com>
2023-12-25 12:17:38 +0100

    added check

--- projectaccessibility/src/i18n.js
+++ projectaccessibility/src/i18n.js
@@ -1,39 +1,27 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import en from "./lang/en";
+import nl from "./lang/nl";
 
-const modules = require.context("./lang", true, /\.json$/);
-
-let translations = {};
-
-// Loop over the keys (paths) in the modules object
-for (const path in modules) {
-    // Extract the language and namespace from the path
-    const match = path.match(/\.\/translations\/(\w+)\/(\w+)\.json$/);
-    if (match) {
-        const lang = match[1];
-        const namespace = match[2];
-        if (!translations[lang]) {
-            translations[lang] = {};
-        }
-        // Get the default export of the module and add it to the translations object
-        translations[lang][namespace] = modules[path].default;
-    }
-}
-
-i18n.use(initReactI18next)
-    .use(LanguageDetector)
-    .init({
-        resources: translations,
-        detection: {
-            order: ["localStorage", "navigator"],
-            caches: ["localStorage"],
-            lookupLocalStorage: "i18nextLng",
+i18n.use(initReactI18next).use(LanguageDetector).init({
+    resources: {
+        en: {
+            translation: en,
         },
-        fallbackLng: "nl",
-        interpolation: {
-            escapeValue: false,
+        nl: {
+            translation: nl,
         },
-    });
+    },
+    lng: localStorage.getItem("language") || "nl",
+    fallbackLng: "nl",
+    detection: {
+        order: ["localStorage"],
+        caches: ["localStorage", "cookie"],
+    },
+    interpolation: {
+        escapeValue: false,
+    },
+});
 
 export default i18n;
