c2899363eb337a81ef8770c60a25c1a43aa2a4d5
Junyi Xie <yunyi.xie@outlook.com>
2023-12-27 15:12:02 +0100

    Fix mobile signup

--- projectaccessibility/src/pages/Signup.jsx
+++ projectaccessibility/src/pages/Signup.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { FormSignup } from "../components/Form";
 
@@ -13,13 +14,15 @@ const Signup = () => {
                 <div className="signup__page_side_banner signup__page_column" />
                 <div className="signup__page_registration signup__page_column">
                     <div className="signup__page_registration__content">
-                        <h2 className="signup__page_registration__title">{translate("signup.pageTitle")}</h2>
-                        <div className="signup__page_registration__form_signup">
-                            <FormSignup />
-                        </div>
-                        <p className="signup__page_registration__login">
-                            {translate("signup.alreadyHaveAccount")} <a href="/login">{translate("signup.logIn")}</a>
-                        </p>
+                        <Container>
+                            <h2 className="signup__page_registration__title">{translate("signup.pageTitle")}</h2>
+                            <div className="signup__page_registration__form_signup">
+                                <FormSignup />
+                            </div>
+                            <p className="signup__page_registration__login">
+                                {translate("signup.alreadyHaveAccount")} <a href="/login">{translate("signup.logIn")}</a>
+                            </p>
+                        </Container>
                     </div>
                 </div>
             </div>
