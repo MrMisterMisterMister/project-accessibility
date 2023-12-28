39d5e621c6d69cf9a4fb9e57c4fbb0b9713e041c
Junyi Xie <yunyi.xie@outlook.com>
2023-12-28 22:28:10 +0100

    setup the basic lines for dashboard

--- projectaccessibility/src/pages/Dashboard.jsx
+++ projectaccessibility/src/pages/Dashboard.jsx
@@ -1,21 +1,49 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
-// t
+// Test
 const Dashboard = () => {
     return (
         <div className="dashboard__page">
-            <div className="dashboard__sidebar">
-                <div className="dashboard__sidebar_inner">
-                    <div className="dashboard__sidebar_heading">
-                        <img src="/img/brand/logo_white_text_light.png" />
-                    </div>
-                    <div className="dashboard__sidebar_nav">
-                        {/* load nav */}
-                    </div>
-                </div>
+            <div className="dashboard__page_menu">
+                <nav className="dashboard__page_menu__topnav">
+                    <Container>
+                        <div className="dashboard__page_menu__wrapper">
+                            <div className="dashboard__page_menu__brand">
+                                <img src="/img/brand/logo_white_text_dark.png" height="60px" width="" />
+                            </div>
+                            <div className="dashboard__page_menu__user">
+                                {/* profile goes here */}
+                            </div>
+                        </div>
+                    </Container>
+                </nav>
+                <nav className="dashboard__page_menu__bottomnav">
+                    <Container>
+                        <ul className="nav__dashboard_menu__nav">
+                            <li className="nav__dashboard_menu__nav_item">
+                                <a className="nav__dashboard_menu__nav_link" href="">
+                                    <i></i>
+                                    <span className="nav__dashboard_menu__nav_title">Test</span>
+                                </a>
+                            </li>
+                            <li className="nav__dashboard_menu__nav_item">
+                                <a className="nav__dashboard_menu__nav_link" href="">
+                                    <i></i>
+                                    <span className="nav__dashboard_menu__nav_title">Test 2</span>
+                                </a>
+                            </li>
+                            <li className="nav__dashboard_menu__nav_item">
+                                <a className="nav__dashboard_menu__nav_link" href="">
+                                    <i></i>
+                                    <span className="nav__dashboard_menu__nav_title">Test 3</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </Container>
+                </nav>
             </div>
-            <div className="dashboard__content">
-                {/* need to load header and footer for dashboard */}
+            <div className="">
             </div>
         </div>
     );
