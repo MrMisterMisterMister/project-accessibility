13a82de558bfecc406a9be784d372e92f15fad61
Junyi Xie <yunyi.xie@outlook.com>
2023-12-28 16:15:15 +0100

    Update 404.jsx

--- projectaccessibility/src/pages/404.jsx
+++ projectaccessibility/src/pages/404.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { Container } from "react-bootstrap";
 
 // 404 page
 // will be fixed later
 const PageNotFound = () => {
     return (
-        <div>
-            <Container>
-                <h1>Your mom</h1>
-            </Container>
-        </div>
+        <h1>Your mom could not be found.</h1>
     );
 };
 
