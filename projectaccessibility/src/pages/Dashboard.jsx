0e16e77d39a06e2144a9f1b9548e74d425e7d589
Junyi Xie <yunyi.xie@outlook.com>
2023-12-29 14:37:54 +0100

    added fixed nav

--- projectaccessibility/src/pages/Dashboard.jsx
+++ projectaccessibility/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import { BarChart, PersonCircle, Gear } from "react-bootstrap-icons";
 import { NavDashboardBottomNav, NavDashboardTopNav } from "../components/Nav";
@@ -25,9 +25,34 @@ const Dashboard = () => {
         }
     ];
 
+    // State to keep track whether of scrolling behaviour done by user
+    const [isScrolling, setIsScrolling] = useState(false);
+
+    // This effect checks if the nav should be fixed to stay visible while the user is scrolling
+    useEffect(() => {
+        const scrollHandler = () => {
+            const scrollPosition = window.scrollY;
+            setIsScrolling(scrollPosition > 90); // Update the state based on scroll position (90 is the height of navtop)
+        };
+
+        window.addEventListener("scroll", scrollHandler);
+
+        return () => {
+            window.removeEventListener("scroll", scrollHandler);
+        };
+    }, []);
+
+    // All the different components need to be loaded in the main
+    // The page will not refresh, but instead just replace the current content on screen
+    // This way the user can perform tasks like, changing their settings, their account information or joining a research
+    // The components that will be loaded depend on the user type
+    // For example:
+    // A panelmember doesn't have a list to view all panelmembers, companies or researches
+    // A company can't change a panelmember account information, and so on
+    // First need to determine what kind of user is logged in from backend or something
     return (
         <div className="dashboard__page">
-            <div className="dashboard__page_menu">
+            <div className={`dashboard__page_menu ${isScrolling ? "fixed__scroll" : ""}`}>
                 <NavDashboardTopNav
                     profilePicturePath="/img/placeholder.jpg"
                     profilePictureAlt="Clodsire"
@@ -35,36 +60,39 @@ const Dashboard = () => {
                 />
                 <NavDashboardBottomNav items={items} />
             </div>
-            {/* content */}
-            <div className="dashboard__page_content">
+            <main className="dashboard__page_main">
                 <Container>
-                    <h1>Welcome, Your mom</h1>
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
+                    <div className="dashboard__page_content">
+                        <h1>Welcome, Your mom</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum modi sit debitis a corrupti atque excepturi pariatur ea, veritatis est aut alias neque blanditiis esse recusandae autem beatae placeat dolorum!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum modi sit debitis a corrupti atque excepturi pariatur ea, veritatis est aut alias neque blanditiis esse recusandae autem beatae placeat dolorum!</p>
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                    </div>
                 </Container>
-            </div>
+            </main>
         </div>
     );
 };
