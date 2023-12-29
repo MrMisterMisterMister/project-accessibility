1c76fea110349cc6eff4779e2bc680209a912ccf
Christopher Yeung <jackknife29jk@gmail.com>
2023-12-29 14:41:59 +0100

    revert commit
    revert commit

--- projectaccessibility/src/components/Button.jsx
+++ projectaccessibility/src/components/Button.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Button } from "react-bootstrap";
-import axiosClient from "../api/axiosClient";
 
 // Primary button. To be made
 const ButtonPrimary = () => {
@@ -76,63 +75,9 @@ ButtonLogin.propTypes = {
 // Like facebook or google
 // Also for create account
 // The icon can be svg
-const ButtonAuth = ({ path, icon, text, provider }) => {
-    const handleSignIn = async () => {
-        switch (provider) {
-            case "google":
-                handleGoogleSignIn();
-                break;
-            case "microsoft":
-                handleMicrosoftSignIn();
-                break;
-            // idk if we need more
-            default:
-                break;
-        }
-    };
-
-    // Get request
-    async function getGoogleClientId() {
-    try {
-        const response = await axiosClient.getRequest('GoogleSignIn/googleClientId');
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching client ID:', error);
-        throw error;
-    }
-}
-
-    const handleGoogleSignIn = async () => {
-        try {
-            // Fetch the client ID from the server
-            const { clientId } = await getGoogleClientId();
-    
-            // Use the obtained client ID for Google sign-in
-            const auth2 = window.gapi.auth2.getAuthInstance({
-                client_id: clientId,
-            });
-    
-            auth2.signIn()
-                .then((googleUser) => {
-                    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-                    // Perform actions after successful sign-in
-                })
-                .catch((error) => {
-                    console.error('Sign-in failed: ' + error);
-                    // sign-in failure
-                });
-        } catch (error) {
-            console.error('Error fetching client ID:', error);
-            // Handle error fetching client ID
-        }
-    };
-
-    const handleMicrosoftSignIn = () => {
-        // Logic for Microsoft authentication
-        // implement Microsoft authentication flow here
-    };
+const ButtonAuth = ({ path, icon, text }) => {
     return (
-        <Button className="button__auth" type="button" href={path} onClick={handleSignIn}>
+        <Button className="button__auth" type="button" href={path}>
             <div className="button__auth_icon">{icon}</div>
             <span className="button__auth_text">{text}</span>
         </Button>
@@ -143,8 +88,7 @@ const ButtonAuth = ({ path, icon, text, provider }) => {
 ButtonAuth.propTypes = {
     path: PropTypes.string.isRequired,
     icon: PropTypes.object.isRequired,
-    text: PropTypes.string.isRequired,
-    provider: PropTypes.string,
+    text: PropTypes.string.isRequired
 };
 
 export { ButtonPrimary, ButtonSecondary, ButtonHero, ButtonContact, ButtonSignup, ButtonLogin, ButtonAuth };
