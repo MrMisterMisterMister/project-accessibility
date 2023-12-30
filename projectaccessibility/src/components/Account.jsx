9b02c37e1aea504b8de3b12a12cbfc90278e8842
Junyi Xie <yunyi.xie@outlook.com>
2023-12-30 11:46:56 +0100

    some additional fields

--- projectaccessibility/src/components/Account.jsx
+++ projectaccessibility/src/components/Account.jsx
@@ -15,29 +15,36 @@ const AccountPanelmember = () => {
                 </div>
             </div>
             <div className="account__dashboard_additional">
-                <div className="">
-                    <h4>General</h4>
-                    <div className="">
-                        <span className="">Birthday:</span>
-                        <span className="">December 30, 2023</span>
-                    </div>
-                    <div className="">
-                        <span className="">Country:</span>
-                        <span className="">The Netherlands</span>
-                    </div>
-                    <div className="">
-                        <span className="">Language:</span>
-                        <span className="">English</span>
-                    </div>
-                    <h4>Contact</h4>
-                    <div className="">
-                        <span className="">Phone:</span>
-                        <span className="">+31 (0)6 123 45 678</span>
-                    </div>
-                    <div className="">
-                        <span className="">Email:</span>
-                        <span className="">your@mom.com</span>
-                    </div>
+                <h4 className="account__dashboard_additional__title">General</h4>
+                <div className="account__dashboard_additional__info">
+                    <span className="account__dashboard_additional__info_label">Birthday:</span>
+                    <span className="account__dashboard_additional__info_value">December 30, 2023</span>
+                </div>
+                <div className="account__dashboard_additional__info">
+                    <span className="account__dashboard_additional__info_label">Country:</span>
+                    <span className="account__dashboard_additional__info_value">The Netherlands</span>
+                </div>
+                <div className="account__dashboard_additional__info">
+                    <span className="account__dashboard_additional__info_label">Language:</span>
+                    <span className="account__dashboard_additional__info_value">English</span>
+                </div>
+                <div className="account__dashboard_additional__info">
+                    <span className="account__dashboard_additional__info_label">Address:</span>
+                    <span className="account__dashboard_additional__info_value">Streetname 123, 5678 BC</span>
+                </div>
+                <h4 className="account__dashboard_additional__title">Contacts</h4>
+                <div className="account__dashboard_additional__info">
+                    <span className="account__dashboard_additional__info_label">Phone:</span>
+                    <span className="account__dashboard_additional__info_value">+31 (0)6 123 45 678</span>
+                </div>
+                <div className="account__dashboard_additional__info">
+                    <span className="account__dashboard_additional__info_label">Email:</span>
+                    <span className="account__dashboard_additional__info_value">your@mom.com</span>
+                </div>
+                <h4 className="account__dashboard_additional__title">Assistive Tools</h4>
+                <div className="account__dashboard_additional__info">
+                    <span className="account__dashboard_additional__info_label">Tool(s):</span>
+                    <span className="account__dashboard_additional__info_value">Screenreader and Voice command software</span>
                 </div>
             </div>
         </div>
