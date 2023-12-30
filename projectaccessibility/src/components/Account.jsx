00ca5745d3414e3c49ad7a1bf600fb71977b278d
Junyi Xie <yunyi.xie@outlook.com>
2023-12-30 11:24:12 +0100

    account

--- projectaccessibility/src/components/Account.jsx
+++ projectaccessibility/src/components/Account.jsx
@@ -2,19 +2,43 @@ import React from "react";
 
 // Account component for panelmember
 const AccountPanelmember = () => {
+    // All the fields are hardcoded for now
+    // This will be removed and added as property once I figure out how to get the user guid after they logged in
+    // I will also most likely merge both panelmember and company into 1 view, and load the data automatically
     return (
         <div className="account__dashboard">
             <div className="account__dashboard_user">
-                <div className="account__dashboard_user__picture">
-                    <img src="/img/placeholder.jpg" />
-                </div>
+                <img className="account__dashboard_user__picture" src="/img/placeholder.jpg" alt="Clodsire" />
                 <div className="account__dashboard_user__info">
                     <span className="account__dashboard_user__name">Your Mom</span>
-                    <p className="account__dashboard_user__biograph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusamus veritatis pariatur tenetur delectus veniam ducimus officiis suscipit dolore nisi! Velit eligendi ad voluptatibus eius, voluptatem consectetur laudantium asperiores. Odit.</p>
+                    <p className="account__dashboard_user__biography">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusamus veritatis pariatur tenetur delectus veniam ducimus officiis suscipit dolore nisi!</p>
                 </div>
             </div>
-            <div className="account__dashboard_additional__info">
-                like birthday, country, etc
+            <div className="account__dashboard_additional">
+                <div className="">
+                    <h4>General</h4>
+                    <div className="">
+                        <span className="">Birthday:</span>
+                        <span className="">December 30, 2023</span>
+                    </div>
+                    <div className="">
+                        <span className="">Country:</span>
+                        <span className="">The Netherlands</span>
+                    </div>
+                    <div className="">
+                        <span className="">Language:</span>
+                        <span className="">English</span>
+                    </div>
+                    <h4>Contact</h4>
+                    <div className="">
+                        <span className="">Phone:</span>
+                        <span className="">+31 (0)6 123 45 678</span>
+                    </div>
+                    <div className="">
+                        <span className="">Email:</span>
+                        <span className="">your@mom.com</span>
+                    </div>
+                </div>
             </div>
         </div>
     );
