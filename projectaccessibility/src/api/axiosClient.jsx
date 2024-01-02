1af365a7f98ba410efc2b7e459f8a2c4d0b53e69
Junyi Xie <yunyi.xie@outlook.com>
2024-01-02 11:54:45 +0100

    handle cookie

--- projectaccessibility/src/api/axiosClient.jsx
+++ projectaccessibility/src/api/axiosClient.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const axiosClient = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true, // This is to handle cookies
     timeout: 3000
 });
 
