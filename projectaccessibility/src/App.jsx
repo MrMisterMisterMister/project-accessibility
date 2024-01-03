0c7a0fb733b638f94f4479d4e1c0d82683911f7a
Christopher Yeung <jackknife29jk@gmail.com>
2024-01-03 12:31:14 +0100

    google client id axios

--- projectaccessibility/src/App.jsx
+++ projectaccessibility/src/App.jsx
@@ -1,30 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Pages from "./pages";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { jwtDecode } from "jwt-decode";
+import axios from "axios";
 
 function App () {
+    const [clientId, setClientId] = useState(null);
 
-    function handleCallbackResponse(response){
+    function handleCallbackResponse (response) {
         console.log("Encoded JWT ID token: " + response.credential);
-        var userObject = jwtDecode(response.credential);
+        const userObject = jwtDecode(response.credential);
         console.log(userObject);
     }
 
     useEffect(() => {
-        /* global goole */
-        google.accounts.id.initialize({
-            client_id: "207599687687-b8qecsbfsauc1p6orj6266lgcl5p169d.apps.googleusercontent.com",
-            callback: handleCallbackResponse
-        });
+        async function fetchGoogleClientId () {
+            try {
+                const response = await axios.get("http://localhost:5000/GoogleSignIn/googleClientId");
+                setClientId(response.data.clientId);
 
-        google.accounts.id.renderButton(
-            document.getElementById("signInDiv"),
-            {theme: "outline", size: "large"}
-        );
+                /* global goole */
+                google.accounts.id.initialize({
+                    client_id: response.data.clientId,
+                    callback: handleCallbackResponse
+                });
 
+                google.accounts.id.renderButton(
+                    document.getElementById("signInDiv"),
+                    { theme: "outline", size: "large", locale: "nl"}
+                );
+
+                google.accounts.id.prompt();
+
+            } catch (error) {
+                console.log("Error axiosing cleint ID: " + error);
+            }
+        }
+
+        fetchGoogleClientId();
     }, []);
+
+    var accessToken = gapi.auth.getToken().access_token;
 
     return (
         <Router>
