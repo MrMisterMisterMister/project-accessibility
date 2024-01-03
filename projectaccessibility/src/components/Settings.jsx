caef838ac145107e15c66193c9e34b1f07f52fa0
Junyi Xie <yunyi.xie@outlook.com>
2024-01-04 00:32:42 +0100

    last one

--- projectaccessibility/src/components/Settings.jsx
+++ projectaccessibility/src/components/Settings.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import {
     FormUserEmailUpdate,
     FormUserPasswordUpdate,
@@ -9,9 +10,12 @@ import {
 // Settings component for panelmembers to change their profile information
 // This loads in their form with the correct post endpoint to the backend server
 const SettingsPanelMember = () => {
+    // Translation
+    const { t: translate } = useTranslation("settings");
+
     return (
         <div className="settings__dashboard">
-            <h4 className="settings__dashboard_title">Panelmember</h4>
+            <h4 className="settings__dashboard_title">{translate("panelMemberProfileUpdateTitle")}</h4>
             <div className="settings__dashboard_content">
                 <FormPanelMemberProfileUpdate />
             </div>
@@ -21,9 +25,12 @@ const SettingsPanelMember = () => {
 
 // And this one is for company to update their profile information
 const SettingsCompany = () => {
+    // Translation
+    const { t: translate } = useTranslation("settings");
+
     return (
         <div className="settings__dashboard">
-            <h4 className="settings__dashboard_title">Company</h4>
+            <h4 className="settings__dashboard_title">{translate("companyProfileUpdateTitle")}</h4>
             <div className="settings__dashboard_content">
                 <FormCompanyProfileUpdate />
             </div>
@@ -33,9 +40,12 @@ const SettingsCompany = () => {
 
 // General form to change email
 const SettingsEmail = () => {
+    // Translation
+    const { t: translate } = useTranslation("settings");
+
     return (
         <div className="settings__dashboard">
-            <h4 className="settings__dashboard_title">Change Your Email</h4>
+            <h4 className="settings__dashboard_title">{translate("emailUpdateTitle")}</h4>
             <div className="settings__dashboard_content">
                 <FormUserEmailUpdate />
             </div>
@@ -46,9 +56,12 @@ const SettingsEmail = () => {
 // Component to change password for a user
 // Sends post request to backend and it verifies the request by looking if the password matches
 const SettingsPassword = () => {
+    // Translation
+    const { t: translate } = useTranslation("settings");
+
     return (
         <div className="settings__dashboard">
-            <h4 className="settings__dashboard_title">Change Your Password</h4>
+            <h4 className="settings__dashboard_title">{translate("passwordUpdateTitle")}</h4>
             <div className="settings__dashboard_content">
                 <FormUserPasswordUpdate />
             </div>
