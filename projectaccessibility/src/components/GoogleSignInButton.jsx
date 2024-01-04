175aa01219f8de197c62bb7b1b9756cb5535c275
Christopher Yeung <jackknife29jk@gmail.com>
2024-01-04 17:18:11 +0100

    Added useAuth from the AuthProvider component
    Tell me if it's wrongly used

--- projectaccessibility/src/components/GoogleSignInButton.jsx
+++ projectaccessibility/src/components/GoogleSignInButton.jsx
@@ -1,18 +1,23 @@
 import React, { useEffect } from "react";
 import { jwtDecode } from "jwt-decode";
+import { useAuth } from "../provider/authProvider";
 
 const CLIENT_ID = "207599687687-b8qecsbfsauc1p6orj6266lgcl5p169d.apps.googleusercontent.com";
 
 function GoogleSignInButton () {
+    const { setToken } = useAuth();
+
     // userobject is the decoded JWT token.
     function handleCallbackResponse (response) {
         console.log("Encoded JWT ID token: " + response.credential);
         const userObject = jwtDecode(response.credential);
         console.log(userObject);
+
+        // Set the token in the authProvider
+        setToken(response.credential);
     }
 
     useEffect(() => {
-        // Tells the lint that we are using a global variable from Google in  index.hmtl
         // Inintialize starts the auth flow
         const google = window.google;
         google.accounts.id.initialize({
@@ -26,11 +31,9 @@ function GoogleSignInButton () {
             { theme: "outline", size: "large", width: 1000, locale: "nl" }
         );
 
-        // Access Token
-
         // Prompt the user to select a Google account to sign in with
         google.accounts.id.prompt();
-    }, []);
+    }, [setToken]);
 
     return (
         <div id="signInDiv"></div>
