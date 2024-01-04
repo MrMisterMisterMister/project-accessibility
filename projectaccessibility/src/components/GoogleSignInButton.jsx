4f1c93d3c687f1e4a609e1dca7a0f0ed690ab2d4
Christopher Yeung <jackknife29jk@gmail.com>
2024-01-04 16:53:46 +0100

    added some comments

--- projectaccessibility/src/components/GoogleSignInButton.jsx
+++ projectaccessibility/src/components/GoogleSignInButton.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from "react";
 import { jwtDecode } from "jwt-decode";
 
+const CLIENT_ID = "207599687687-b8qecsbfsauc1p6orj6266lgcl5p169d.apps.googleusercontent.com";
+
 function GoogleSignInButton () {
 
+    // userobject is the decoded JWT token. 
     function handleCallbackResponse (response) {
         console.log("Encoded JWT ID token: " + response.credential);
         var userObject = jwtDecode(response.credential);
@@ -10,17 +13,24 @@ function GoogleSignInButton () {
     }
 
     useEffect(() => {
+        // Tells the lint that we are using a global variable from Google in  index.hmtl
+        // Inintialize starts the auth flow
         /* global google */
+        const google = window.google;
         google.accounts.id.initialize({
-            client_id: "207599687687-b8qecsbfsauc1p6orj6266lgcl5p169d.apps.googleusercontent.com",
+            client_id: CLIENT_ID,
             callback: handleCallbackResponse
         });
 
+        // Renders the button
         google.accounts.id.renderButton(
             document.getElementById("signInDiv"),
             { theme: "outline", size: "large", width:1000, locale: "nl" }
         );
 
+        // Access Token
+
+        // Prompt the user to select a Google account to sign in with
         google.accounts.id.prompt();
 
     }, []);
