923c6eea2b8c01b12f6d63b5fe39d152c61273ec
Christopher Yeung <jackknife29jk@gmail.com>
2024-01-04 16:58:46 +0100

    npm run lint

--- projectaccessibility/src/components/GoogleSignInButton.jsx
+++ projectaccessibility/src/components/GoogleSignInButton.jsx
@@ -4,18 +4,16 @@ import { jwtDecode } from "jwt-decode";
 const CLIENT_ID = "207599687687-b8qecsbfsauc1p6orj6266lgcl5p169d.apps.googleusercontent.com";
 
 function GoogleSignInButton () {
-
-    // userobject is the decoded JWT token. 
+    // userobject is the decoded JWT token.
     function handleCallbackResponse (response) {
         console.log("Encoded JWT ID token: " + response.credential);
-        var userObject = jwtDecode(response.credential);
+        const userObject = jwtDecode(response.credential);
         console.log(userObject);
     }
 
     useEffect(() => {
         // Tells the lint that we are using a global variable from Google in  index.hmtl
         // Inintialize starts the auth flow
-        /* global google */
         const google = window.google;
         google.accounts.id.initialize({
             client_id: CLIENT_ID,
@@ -25,14 +23,13 @@ function GoogleSignInButton () {
         // Renders the button
         google.accounts.id.renderButton(
             document.getElementById("signInDiv"),
-            { theme: "outline", size: "large", width:1000, locale: "nl" }
+            { theme: "outline", size: "large", width: 1000, locale: "nl" }
         );
 
         // Access Token
 
         // Prompt the user to select a Google account to sign in with
         google.accounts.id.prompt();
-
     }, []);
 
     return (
