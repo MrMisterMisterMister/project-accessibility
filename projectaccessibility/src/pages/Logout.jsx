38cfe60806145f39e6731a4848e1ddcfd50430f1
MrMisterMisterMister <114733626+MrMisterMisterMister@users.noreply.github.com>
2024-01-08 11:12:14 +0100

    updated logout

--- projectaccessibility/src/pages/Logout.jsx
+++ projectaccessibility/src/pages/Logout.jsx
@@ -3,10 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { postRequest } from "../api/axiosClient";
 import { Cookies } from "js-cookie";
 
-// TESTING ONLY, will be modified later
-// very simple logout
-// this will not be how we handle it actually, but it's for testing only
-// TODO
 // When a user decides to logout
 // Send a post request to api server, which then deletes/revokes the cookie and the refreshtoken
 const Logout = () => {
