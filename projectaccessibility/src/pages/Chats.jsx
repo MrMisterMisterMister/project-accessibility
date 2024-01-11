08316756ee96d3eee71feb983d75f93c9a06ff00
Christopher Yeung <jackknife29jk@gmail.com>
2024-01-11 08:59:16 +0100

    chatt

--- projectaccessibility/src/pages/Chats.jsx
+++ projectaccessibility/src/pages/Chats.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 // TODO
 // Chat view with SignalR?
 const Chats = () => {
-    return <h1><s>Your mom.</s>Chats</h1>;
+    return (
+        <div>
+            <h1>Chats</h1>
+            <main>
+                <Container>
+                    <Row class='px-5 my-5'>
+                        <Col sm='12'>
+                            <h1 className="font-weight-light">Welcome to the chat room!</h1>
+                        </Col>
+                    </Row>
+                </Container>    
+            </main>
+        </div>
+    );
 };
 
 export default Chats;
