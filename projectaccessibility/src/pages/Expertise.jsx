2ed4c1b72399fe7c2aa126aa2a9323e7b121e434
Junyi Xie <yunyi.xie@outlook.com>
2024-01-12 00:13:06 +0100

    Update Expertise.jsx

--- projectaccessibility/src/pages/Expertise.jsx
+++ projectaccessibility/src/pages/Expertise.jsx
@@ -21,10 +21,10 @@ const Expertise = () => {
                     <h1 tabIndex="0" className="expertise__page_title">{translate("pageTitle")}</h1>
                     <p tabIndex="0" className="expertise__page_intro">{translate("intro")}</p>
                     {translate("cards", { returnObjects: true }).map((key, index) => (
-                        <>
+                        <React.Fragment key={index}>
                             <h4 tabIndex="0">{translate(`cards.${index}.title`)}</h4>
                             <p tabIndex="0">{translate(`cards.${index}.text`)}</p>
-                        </>
+                        </React.Fragment>
                     ))}
                 </Container>
             </div>
