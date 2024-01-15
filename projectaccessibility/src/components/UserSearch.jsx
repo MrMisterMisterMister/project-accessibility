a47857eaaa3622453701cbc726cbebccbe09fe89
Christopher Yeung <jackknife29jk@gmail.com>
2024-01-15 08:48:03 +0100

    for now

---
+++ projectaccessibility/src/components/UserSearch.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { createEndpoint } from "../api/axiosClient";
+import { Input, Button } from 'react-chat-elements';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+const UserSearch = ({ onSelectUser, setSearchResults, searchResults }) => {
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSearch = async () => {
+        try {
+            const data = await createEndpoint(`users/search/${searchQuery}`).get();
+            setSearchResults(data);
+        } catch (error) {
+            console.error('Error during search:', error);
+        }
+    };
+
+    // Handle change in search query
+    const handleChange = (e) => {
+        setSearchQuery(e.target.value);
+        if (e.target.value.trim() === '') {
+            setSearchResults([]);
+        }
+    };
+
+    return (
+        <div>
+            <Input
+                placeholder="Search for users..."
+                value={searchQuery}
+                onChange={handleChange}
+                rightButtons={
+                    <Button text="Search" onClick={handleSearch} />
+                }
+            />
+
+            <ListGroup>
+                {searchResults && searchResults.map(user => (
+                    <ListGroup.Item action key={user.id} onClick={() => onSelectUser(user)}>
+                        {user.name} ({user.email})
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>
+
+        </div>
+    );
+};
+
+export default UserSearch;
--- projectaccessibility/src/pages/Chats.jsx
+++ projectaccessibility/src/pages/Chats.jsx
@@ -1,31 +1,35 @@
-import React, { useState, useEffect, useRef } from "react";
-import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
-import { Col, Container, Row, Form, Button } from "react-bootstrap";
+import React, { useState, useEffect, useRef } from 'react';
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+import { Col, Container, Row, Form } from 'react-bootstrap';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
+import { createEndpoint } from '../api/axiosClient';
+import UserSearch from '../components/UserSearch';
+import { MessageList, MessageBox, ChatList, Input, Button } from 'react-chat-elements';
+import "react-chat-elements/dist/main.css"
 
-import { useStore } from "../stores/store";
-import { observer } from "mobx-react-lite";
-import { createEndpoint } from "../api/axiosClient";
-
-const Chats = observer( () => {
+const Chats = observer(() => {
+    // For user 
     const { userStore: { user, getUser } } = useStore();
     const [currentUser, setCurrentUser] = useState({});
+    // For chat
     const [connection, setConnection] = useState(null);
-    const [chatRoom, setChatRoom] = useState('');
     const [messages, setMessages] = useState([]);
+
     const [newMessage, setNewMessage] = useState('');
     const latestMessages = useRef(null);
+
     const [isConnected, setIsConnected] = useState(false);
 
-    // Set the latest messages to the messages state
+    const [searchResults, setSearchResults] = useState([]);
+    const [selectedUser, setSelectedUser] = useState(null);
+
     latestMessages.current = messages;
 
     // Fetch user data
     useEffect(() => {
-        // Fetch user data
         const getData = async () => {
-            // If user is not available or needs to be updated, fetch it
             if (!user) await getUser();
-            // Proceed to fetch additional user data or details
             if (user) {
                 const data = await createEndpoint(`users/${user.userId}`).get();
                 setCurrentUser(data);
@@ -35,93 +39,136 @@ const Chats = observer( () => {
         getData();
     }, [user]);
 
-    // Create the connection to the signalR hub (ChatHub, http://whateverrrr/chat)
+    // SignalR connection
     useEffect(() => {
-        // Make the connection to signalR hub
         const newConnection = new HubConnectionBuilder()
-            .withUrl("http://localhost:5000/chat")
+            .withUrl('http://localhost:5000/chat')
             .configureLogging(LogLevel.Information)
             .build();
-
-        // Set the connection to the state
-        setConnection(newConnection);
-    }, []);
-
-    useEffect(() => {
-        if (connection) { // If connection is not null
-            console.log(currentUser.userName) // Just checking if the username is being passed correctly :) removing this later, not needed
-            connection.start()
-                .then(() => {
-                    console.log('Connected!');
-                    setIsConnected(true);  // Set isConnected to true when the connection is established
-                    connection.on('ReceiveMessage', (username, message) => {
-                        const updatedMessages = [...latestMessages.current];
-                        updatedMessages.push({ username, message });
-
-                        setMessages(updatedMessages);
-                    });
-                })
-                .catch(e => {
-                    console.log('Connection failed: ', e);
-                    setIsConnected(false);
-                });
-        }
-    }, [connection]);
     
-    // Join the chat room function
-    const joinRoom = () => {
-        if (connection && isConnected) {
-            connection.invoke('JoinRoom', { Username: currentUser.userName, ChatRoom: chatRoom })
-                .catch(err => console.error(err));
-        }
+        newConnection.start()
+            .then(() => {
+                console.log('Connected to SignalR Hub');
+                setIsConnected(true);
+    
+                if (currentUser && currentUser.email) {
+                    newConnection.invoke('RegisterUser', currentUser.email);
+                }
+    
+                // In the ReceivePrivateMessage handler
+                newConnection.on('ReceivePrivateMessage', (fromUser, message) => {
+                    setMessages(prevMessages => [...prevMessages, { fromUser, message }]);
+                });
+            })
+            .catch(e => console.error('Connection to SignalR Hub failed: ', e));
+    
+        setConnection(newConnection);
+    
+        return () => {
+            newConnection.stop().then(() => console.log('Disconnected from SignalR Hub'));
+        };
+    }, []);    
+
+    const handleSelectUser = (userResult) => {
+        console.log("Selected User: ", userResult);
+        setSelectedUser(userResult);
+        // Load message history with the selected user
     };
 
-    // Send message to the chat room function
-    const sendMessage = async () => {
-        if (connection && isConnected && newMessage) {
-            await connection.invoke('SendMessageToRoom', chatRoom, currentUser.userName, newMessage);
+    const sendPrivateMessage = async () => {
+        if (connection && isConnected && newMessage && selectedUser) {
+            await connection.invoke('SendMessageToUser', selectedUser.id, newMessage);
             setNewMessage('');
         }
     };
 
+    // Function to transform messages to the format required by MessageList
+    const transformMessages = () => {
+        return messages.map(message => ({
+            position: message.fromUser === currentUser.email ? 'right' : 'left',
+            type: 'text',
+            text: message.message,
+            date: new Date(), // Adjust as per your requirements
+        }));
+    };
+
+    // Inline styles for the chat messages area and the input box
+    const messageListStyle = {
+        height: 'calc(100vh - 150px)', // Adjust the height as needed
+        overflowY: 'auto',
+        marginBottom: '50px' // Space for the input box
+    };
+
+    const inputStyle = {
+        position: 'fixed',
+        bottom: 20,
+        width: '50%', // Adjust the width according to your layout
+        left: '40%'  // Adjust the left position according to your layout
+    };
+
     return (
         <div>
-            <h1>Chats</h1>
-            <main>
-                <Container>
-                    <Row className='px-5 my-5'>
-                        <Col sm='12'>
-                            <h1 className='font-weight-light'>Welcome to {chatRoom} chat room!</h1>
-                            <Form.Group>
-                                <Form.Control
-                                    type='text'
-                                    placeholder='Enter chat room name...'
-                                    value={chatRoom}
-                                    onChange={(e) => setChatRoom(e.target.value)}
-                                />
-                                <Button onClick={joinRoom}>Join Room</Button>
-                            </Form.Group>
-
-                            <Form.Group>
-                                <Form.Control
-                                    as='textarea'
-                                    rows={3}
-                                    placeholder='Enter your message...'
-                                    value={newMessage}
-                                    onChange={(e) => setNewMessage(e.target.value)}
-                                />
-                                <Button onClick={sendMessage}>Send Message</Button>
-                            </Form.Group>
-
-                            <div>
-                                {messages.map((message, index) => (
-                                    <div key={index}><strong>{message.username}</strong>: {message.message}</div>
-                                ))}
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </main>
+            <Container>
+                <Row>
+                    <Col sm='4'>
+                        <UserSearch onSelectUser={handleSelectUser} setSearchResults={setSearchResults} searchResults={searchResults} />
+                        <br />
+                        <ChatList
+                            className='chat-list'
+                            dataSource={[
+                                {
+                                avatar: 'https://avatars.githubusercontent.com/u/80540635?v=4',
+                                alt: 'kursat_avatar',
+                                title: 'Kursat',
+                                subtitle: "Why don't we go to the No Way Home movie this weekend ?",
+                                date: new Date(),
+                                unread: 3,
+                                },
+                                {
+                                avatar: 'https://avatars.githubusercontent.com/u/80540635?v=4',
+                                alt: 'kursat_avatar',
+                                title: 'Kursat',
+                                subtitle: "Why don't we go to the No Way Home movie this weekend ?",
+                                date: new Date(),
+                                unread: 3,
+                                }
+                            ]} />
+                    </Col>
+                    <Col sm='8'>
+                        <div style={messageListStyle} className='message-list'>
+                            <MessageList
+                                className='message-list'
+                                lockable={true}
+                                toBottomHeight={'100%'}
+                                dataSource={[
+                                    {
+                                      position:"left",
+                                      type:"text",
+                                      title:"Kursat",
+                                      text:"Give me a message list example !",
+                                    },
+                                    {
+                                      position:"right",
+                                      type:"text",
+                                      title:"Emre",
+                                      text:"That's all.",
+                                    },
+                                    ]} // Use transformed messages (transformMessages) here
+                            />
+                        </div>
+                        <div style={inputStyle}>
+                            <Input
+                                placeholder="Type a message..."
+                                value={newMessage}
+                                onChange={(e) => setNewMessage(e.target.value)}
+                                rightButtons={
+                                    <Button onClick={sendPrivateMessage} color='white' backgroundcolor='black' text='Send'/>
+                                }
+                            />
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 });
