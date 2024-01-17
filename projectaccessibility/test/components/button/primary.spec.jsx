746124e8d86fe720f9c28f9b7ebf8659d6a87474
MrMisterMisterMister <22003908@student.hhs.nl>
2024-01-17 19:29:48 +0100

    login test

--- projectaccessibility/test/components/button/primary.spec.jsx
+++ projectaccessibility/test/components/button/primary.spec.jsx
@@ -3,13 +3,14 @@ import { ButtonPrimary } from "../../../src/components/Button";
 import { fireEvent, render } from "@testing-library/react";
 
 describe("Testing the Primary Button", () => {
-    test("renders correctly", () => {
+    test("renders correctly when active and clicked", () => {
         // Arrange
-        const text = "Primary";
+        const text = "Primary 1";
         const isActive = true;
         const action = vi.fn();
-        const { getByText } = render(<ButtonPrimary text={text} isActive={isActive} action={action} />);
-        const primaryButton = getByText(text);
+        const primaryButton = render(
+            <ButtonPrimary text={text} isActive={isActive} action={action} />)
+            .getByText(text);
 
         // Act
         fireEvent.click(primaryButton);
@@ -17,5 +18,26 @@ describe("Testing the Primary Button", () => {
         // Assert
         expect(action).toHaveBeenCalled();
         expect(primaryButton).toBeDefined();
+        expect(primaryButton.className).toContain("button__primary");
+        expect(primaryButton.className).toContain("active");
+    });
+
+    test("renders correctly when inactive and clicked", () => {
+        // Arrange
+        const text = "Primary 2";
+        const isActive = false;
+        const action = vi.fn();
+        const primaryButton = render(
+            <ButtonPrimary text={text} isActive={isActive} action={action} />)
+            .getByText(text);
+
+        // Act
+        fireEvent.click(primaryButton);
+
+        // Assert
+        expect(action).toHaveBeenCalled();
+        expect(primaryButton).toBeDefined();
+        expect(primaryButton.className).toContain("button__primary");
+        expect(primaryButton.className).not.toContain("active");
     });
 });
