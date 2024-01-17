eaf2e12e4b026a781030b83972392c45df369d74
Junyi Xie <yunyi.xie@outlook.com>
2024-01-17 13:18:52 +0100

    table panelmember test

---
+++ projectaccessibility/test/components/table/panelmember.spec.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { describe, test, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { TablePanelMemberView } from "../../../src/components/Table";
+
+describe("Testing the Panel Member table view", () => {
+    test("renders correctly with panel member data", () => {
+        // Arrange
+        const data = [{
+            id: 1,
+            firstName: "Essex",
+            lastName: "Essex",
+            email: "essex@email.com",
+            dateOfBirth: "1942-12-31",
+            address: "The street of Essex's",
+            postalCode: "9999CV",
+            city: "City of Essex",
+            country: "Essex of the freedom"
+        }];
+
+        // Act
+        render(<TablePanelMemberView data={data} />);
+
+        // Assert
+        expect(screen.getByText("Essex Essex")).toBeDefined();
+        expect(screen.getByText("essex@email.com")).toBeDefined();
+        expect(screen.getByText("31 december 1942")).toBeDefined();
+        expect(screen.getByText("The street of Essex's")).toBeDefined();
+        expect(screen.getByText("9999CV")).toBeDefined();
+        expect(screen.getByText("City of Essex")).toBeDefined();
+        expect(screen.getByText("Essex of the freedom")).toBeDefined();
+    });
+});
