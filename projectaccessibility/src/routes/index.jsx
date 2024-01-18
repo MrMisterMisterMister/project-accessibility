96dabc8074500e9024ac9c67b1cbc92dba97e3be
BorisBruijstens <Borisbruijstens@gmail.com>
2024-01-18 22:58:13 +0100

    Fixes

--- projectaccessibility/src/routes/index.jsx
+++ projectaccessibility/src/routes/index.jsx
@@ -13,7 +13,6 @@ import PrivacyPolicy from "../pages/PrivacyPolicy";
 import Cookies from "../pages/Cookies";
 import PageNotFound from "../pages/404";
 import Dashboard from "../pages/Dashboard";
-import Contact from "../pages/Contact";
 
 // Route component
 // This here handles all the routing in the application
